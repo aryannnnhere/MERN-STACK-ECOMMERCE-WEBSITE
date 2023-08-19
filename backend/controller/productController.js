cc89df7e17c7a87ec3b4650f79a3f8cd3531c08e
aryannnnhere <aryanskbyadav@gmail.com>
2023-08-19 23:51:57 +0530

    crud operation of product

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,7 +1,7 @@
 const Product = require("../models/productModel");
 
 
-// create Product
+// create Product -- Admin
 exports.createProduct = async (req, res, next)=>{
     const product = await Product.create(req.body);
 
@@ -12,8 +12,51 @@ exports.createProduct = async (req, res, next)=>{
     })
 
 }
+// get All products 
+exports.getAllProducts = async (req, res)=>{
 
-exports.getAllProducts = (req, res)=>{
-
-    res.status(200).json({message: "route is working fine"});
+    const products = await Product.find();
+    res.status(200).json({
+        success: true,
+        products
+       });
 }
+
+// update product - ADMIN
+exports.updateProduct = async (req,res)=>{
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"product Not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id , req.body , {
+        new: true,
+        runValidators:true, 
+        useFindAndModify: false
+     });
+     res.status(200).json({
+        success:true,
+        product
+     })
+}
+
+// delete product 
+ exports.deleteProduct = async (req, res, next)=>{
+    const product = await Product.findById(req.params.id)
+
+    if(!product){return res.status(500).json({
+        success:false,
+        message:"product Not found"
+    })}
+
+    await product.deleteOne();
+    res.status(200).json({
+        success:true,
+        message:"product Deleted Successfully"
+    })
+
+
+ }
